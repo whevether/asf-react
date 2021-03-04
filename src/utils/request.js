6f54247892b7654d7518c880729ecd0ab8a57e45
keep_wan <whevether@outlook.com>
2021-03-04 21:51:14 +0800

    update

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 export const axiosInstance = axios.create({
-  baseURL: process.env.NODE_ENV === 'production' ? 'https://asf.keep-wan.me/api' : '/api'
+  baseURL: process.env.NODE_ENV === 'production' ? '/api' : '/api'
 });
 //设置 token api 请求头
 export const setToken = (token) => {
