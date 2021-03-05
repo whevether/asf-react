4b11e0ad331ec8a0c7782cf5dabafc68252a017e
keep_wan <whevether@outlook.com>
2021-03-05 15:38:35 +0800

    update

--- src/store/configureStore.prod.js
+++ src/store/configureStore.prod.js
@@ -1,64 +1,12 @@
 import {createStore, compose, applyMiddleware} from 'redux';
 import thunkMiddleware from 'redux-thunk';
 import rootReducer from './reducers';
-import {axiosInstance} from 'utils/request';
-import {removeCookie} from 'utils/storage';
-import {notification} from 'antd';
+import {request} from 'utils/request';
 import { routerMiddleware } from 'connected-react-router';
 export default function configureStore(history,initialState) {
-  axiosInstance.interceptors.request.use((config) => {
-    return config;
-  },(error) =>{
-    return Promise.reject(error);
-  });
-  axiosInstance.interceptors.response.use((response)=>{
-    if(response.status === 200 && (response?.data?.status === 200 || response?.data?.status === 0)){
-      return response?.data?.result;
-    }else{
-      notification['error']({
-        message: response?.data.message
-      });
-      return Promise.reject(response?.data);
-    }
-  },(err)=>{
-    const {response} = err;
-    if([401,403].indexOf(response?.status) > -1){
-      notification['error']({
-        message: '请求错误',
-        description:
-          '没有权限'
-      });
-      removeCookie(['token','refreshToken']);
-      // window.location.href = '/login';
-      history.push('/login');
-    }
-    if([400].indexOf(response?.status) > -1){
-      notification['error']({
-        message: '请求错误',
-        description:
-          response?.data.message
-      });
-    }
-    if([500].indexOf(response?.status) > -1){
-      notification['error']({
-        message: '请求错误',
-        description:
-          '服务器错误'
-      });
-      history.push('/500');
-    }
-    if([404].indexOf(response?.status) > -1){
-      notification['error']({
-        message: '请求错误',
-        description:
-          '没有这个接口'
-      });
-    }
-    return Promise.reject(err);
-  });
   const middlewares = [
     routerMiddleware(history),
-    thunkMiddleware.withExtraArgument(axiosInstance)
+    thunkMiddleware.withExtraArgument(request(history))
   ];
   const store = createStore(rootReducer(history), initialState, compose(
     applyMiddleware(...middlewares)
--- src/utils/request.js
+++ src/utils/request.js
@@ -1,8 +1,63 @@
 import axios from 'axios';
-export const axiosInstance = axios.create({
+import {removeCookie} from 'utils/storage';
+import {notification} from 'antd';
+const axiosInstance = axios.create({
   baseURL: process.env.NODE_ENV === 'production' ? '/api' : '/api'
 });
 //设置 token api 请求头
 export const setToken = (token) => {
   axiosInstance.defaults.headers.common.Authorization = `Bearer ${token}`;
+};
+export const request = (history) => {
+  axiosInstance.interceptors.request.use((config) => {
+    return config;
+  },(error) =>{
+    return Promise.reject(error);
+  });
+  axiosInstance.interceptors.response.use((response)=>{
+    if(response.status === 200 && (response?.data?.status === 200 || response?.data?.status === 0)){
+      return response?.data?.result;
+    }else{
+      notification['error']({
+        message: response?.data.message
+      });
+      return Promise.reject(response?.data);
+    }
+  },(err)=>{
+    const {response} = err;
+    if([401,403].indexOf(response?.status) > -1){
+      notification['error']({
+        message: '请求错误',
+        description:
+          '没有权限'
+      });
+      removeCookie(['token','refreshToken']);
+      // window.location.href = '/login';
+      history.push('/login');
+    }
+    if([400].indexOf(response?.status) > -1){
+      notification['error']({
+        message: '请求错误',
+        description:
+          response?.data.message
+      });
+    }
+    if([500].indexOf(response?.status) > -1){
+      notification['error']({
+        message: '请求错误',
+        description:
+          '服务器错误'
+      });
+      history.push('/500');
+    }
+    if([404].indexOf(response?.status) > -1){
+      notification['error']({
+        message: '请求错误',
+        description:
+          '没有这个接口'
+      });
+    }
+    return Promise.reject(err);
+  });
+  return axiosInstance;
 };
