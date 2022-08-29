f37a24b2d32a53e741f8d037ef70f6305dfe71f0
keep_wan <whevether@outlook.com>
2022-08-29 20:54:05 +0800

    update

--- src/components/AuthControl.js
+++ src/components/AuthControl.js
@@ -8,12 +8,12 @@ const AuthControl = (props) => {
             if (props?.type == 'menu') {
                 return (<Menu>
                     {props?.list.map((item, i) => {
-                        return props?.userInfo?.actions?.includes(item.permission) ? <Menu.Item key={i} onClick={() => item?.click(props?.record)} style={(props?.record?.isSystem === 1 && !(props?.userInfo?.roleName?.indexOf('superadmin') > -1 && props?.userInfo?.tenancyId === props?.record?.tenancyId)) ? {pointerEvents: 'none',color: '#ccc',cursor:'not-allowed'} : null}>{item?.name}</Menu.Item> : null;
+                        return props?.userInfo?.actions?.includes(item.permission) ? <Menu.Item key={i} onClick={() => item?.click(props?.record)} style={(props?.record?.isSystem === 1 && !(props?.userInfo?.roleName?.indexOf('superadmin') > -1 && props?.userInfo?.tenancyId === props?.record?.tenancyId)) ? {pointerEvents: 'none',color: '#ccc',cursor:'not-allowed'} : null} {...item?.rest}>{item?.name}</Menu.Item> : null;
                     })}
                 </Menu>);
             } else {
                 return props?.list.map((item, i) => {
-                    return props?.userInfo?.actions?.includes(item.permission) ? <Button key={i} onClick={(e) => item?.click(e)} type={item?.type} icon={item?.icon} loading={item?.loading} size={item?.size} style={{marginLeft:'10px'}}>{item?.name}</Button> : null;
+                    return props?.userInfo?.actions?.includes(item.permission) ? <Button key={i} onClick={() => item?.click(props?.record)} type={item?.type} icon={item?.icon} loading={item?.loading} size={item?.size} style={{marginLeft:'10px',marginBottom: '10px'}} {...item?.rest}>{item?.name}</Button> : null;
 
                 });
             }
