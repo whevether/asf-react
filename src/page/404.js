7eee4e2964f7f6a3eb41502c63a0f7106d39dff9
xdl <xdl7306@163.com>
2022-09-20 12:24:33 +0800

    update

--- src/page/404.js
+++ src/page/404.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import {head} from 'utils/head';
 // import PropTypes from 'prop-types';
-const NotFoundPage = () => {
+const Error404 = () => {
   // staticContext.notFound = true;
   return (<div className="notfound">
     {head('404')}
-    <img src="../../assets/404.svg"/>
+    <img src="assets/404.svg"/>
   </div>);
 };
 // NotFoundPage.propTypes = {
 //   staticContext: PropTypes.object
 // };
-export default NotFoundPage;
+export default Error404;
