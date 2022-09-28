3e6ccd18ab6abe9d331901345efd12015f17fd1c
xdl <xdl7306@163.com>
2022-09-28 10:24:34 +0800

    update

--- src/page/role/details.js
+++ src/page/role/details.js
@@ -1,9 +1,35 @@
-import React from 'react';
-const Details = ()=>{
+import React, { useEffect } from 'react';
+import PropTypes from 'prop-types';
+import * as roleAction from 'store/actions/role';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { useSearchParams } from 'react-router-dom';
+const Details = (props)=>{
+  let [searchParams] = useSearchParams();
+  useEffect(()=>{
+    props?.roleFunc?.detailsRole({id: searchParams.get('id')})
+      .then(res=>{
+        console.log(res);
+      });
+  },[]);
   return (
     <div className="details">
       详情
     </div>
   );
 };
-export default Details;
+Details.propTypes = {
+  roleFunc: PropTypes.object,
+  userInfo: PropTypes.object,
+  role: PropTypes.object,
+  tenancyList: PropTypes.arrayOf(Object)
+};
+export default connect(state => ({
+  userInfo: state?.common?.data,
+  role: state?.role,
+  tenancyList: state?.common?.tenancyList
+}), dispatch => {
+  return {
+    roleFunc: bindActionCreators(roleAction, dispatch)
+  };
+})(Details);
