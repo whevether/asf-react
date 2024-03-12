97f48f86520cb0a340719ec24972e05b55edc889
whevether <whevether@outlook.com>
2024-03-12 22:44:39 +0800

    update

--- src/store/configureStore.prod.js
+++ src/store/configureStore.prod.js
@@ -1,25 +1,17 @@
 import {createStore, compose, applyMiddleware} from 'redux';
-import thunkMiddleware from 'redux-thunk';
+import {withExtraArgument} from 'redux-thunk';
 import rootReducer from './reducers';
 import {request,axiosInstance} from 'utils/request';
-import { createReduxHistoryContext } from "redux-first-history";
-export default function configureStore(history,initialState) {
-  const { createReduxHistory, routerMiddleware, routerReducer } = createReduxHistoryContext({ 
-    history: history,
-    //other options if needed 
-  });
+export default function configureStore(initialState) {
   const middlewares = [
-    routerMiddleware,
-    thunkMiddleware.withExtraArgument(axiosInstance),
+    withExtraArgument(axiosInstance),
   ];
-  const store = createStore(rootReducer(routerReducer), initialState, compose(
+  const store = createStore(rootReducer(), initialState, compose(
     applyMiddleware(...middlewares)
     )
   );
-  const h = createReduxHistory(store);
-  request(h,store);
+  request(store);
   return {
-    store,
-    h
+    store
   };
 }
