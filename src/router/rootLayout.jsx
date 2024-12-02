5522495c9caa01d1a94df0ab519a19e4f602c25c
keep_wan <whevether@outlook.com>
2024-12-02 11:10:54 +0800

    Update rootLayout.jsx

--- src/router/rootLayout.jsx
+++ src/router/rootLayout.jsx
@@ -1,32 +1,45 @@
 import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useNavigate } from 'react-router-dom';
 import { bindActionCreators } from 'redux';
 import * as commonAction from 'store/actions/common';
 import { Spin } from 'antd';
-import {useI18n} from 'utils/i18n';
+import { getCookie } from 'utils/storage';
+import { setToken } from 'utils/request';
+import { useI18n } from 'utils/i18n';
 //总布局
 const RootLayout = (props) => {
+  let navigate = useNavigate();
   useEffect(() => {
-    if(props?.common?.countryList?.length === 0){
+    if (props?.common?.countryList?.length === 0) {
       props?.getCountryList();
     }
     props?.getTranslatetList()
-      .then(res=>{
+      .then(res => {
         useI18n(res);
       });
     //当没有租户列表就获取租户列表
     if (!props?.common?.tenancyList) {
       props?.fetchTenancyList();
     }
+    if (getCookie('token')) {
+      document.getElementsByTagName('body')[0].className = 'login-svg-none';
+      setToken(getCookie('token'));
+      // 当数据存在于 store中就不请求加载
+      if (!props?.common?.data) {
+        props?.fetchUserInfo();
+      }
+    } else {
+      navigate('/login');
+    }
   }, []);
-  const style = { position: 'fixed', display: 'flex', width: '100%', height: '100%', alignItems: 'center', justifyContent: 'center', backgroundColor: 'rgba(0,0,0,.8)', zIndex: 9999,maxHeight: '100%' };
+  const style = { position: 'fixed', display: 'flex', width: '100%', height: '100%', alignItems: 'center', justifyContent: 'center', backgroundColor: 'rgba(0,0,0,.8)', zIndex: 9999, maxHeight: '100%' };
   return (
     <div className="main" >
       <Spin tip="请求中。。。。。" spinning={props?.common?.loading} delay={500} style={style}>
         {
-          props?.common?.tenancyList  && <Outlet />
+          props?.common?.tenancyList && <Outlet />
         }
       </Spin>
     </div>
