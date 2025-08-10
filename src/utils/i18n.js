aca191b5d2a4dd63a6fc55bceb97f854b7ffa17c
whevether <whevether@outlook.com>
2025-08-10 19:32:16 +0800

    update

--- src/utils/i18n.js
+++ src/utils/i18n.js
@@ -1,7 +1,7 @@
-import {use} from 'i18next';
+import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 export const useI18n = (data) => {
-  return use(initReactI18next)
+  return i18n.use(initReactI18next)
     .init({
       lng: 'zh',
       debug: true,
